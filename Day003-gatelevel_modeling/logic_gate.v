9b9b0e04161c9943b9b4377a301e2f4a2ba9e2e8
manish kushwaha <kushwahamanish13068@gmail.com>
2025-07-30 23:57:59 +0530

    Update logic_gate.v

--- Day003-gatelevel_modeling/logic_gate.v
+++ Day003-gatelevel_modeling/logic_gate.v
@@ -1,1 +1,13 @@
+`timescale 1ns / 1ps
 
+module logic_gate(a,b,c1,c2,c3,c4,c5,c6);
+ input a,b;
+ output c1,c2,c3,c4,c5,c6;
+ and M1 (c1,a,b);
+ or M2 (c2,a,b);
+ nand M3 (c3,a,b);
+ nor M4 (c4,a,b);
+ xor M5 (c5,a,b);
+ xnor M6 (c6,a,b);
+
+endmodule
