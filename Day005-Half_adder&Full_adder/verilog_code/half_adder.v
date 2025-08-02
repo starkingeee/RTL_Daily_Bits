6ec212dc8bd8a1766a50cf43fbef9549c42952e2
manish kushwaha <kushwahamanish13068@gmail.com>
2025-08-02 14:07:47 +0530

    Update half_adder.v

--- Day005-Half_adder&Full_adder/verilog_code/half_adder.v
+++ Day005-Half_adder&Full_adder/verilog_code/half_adder.v
@@ -1,1 +1,22 @@
+// logic for half adder
+module half_adder (a,b,sum,cout);
+ input a,b;
+ output wire sum,cout;
+ assign sum=a^b;
+ assign cout= a*b;
+endmodule
 
+// this is testbench for half adder
+ module tb();
+reg a,b;
+wire sum,cout;
+half_adder adder(a,b,sum,cout);
+initial begin 
+ #2 a=0; b=0;
+ #2 a=0; b=1;
+ #2 a=1; b=0;
+ #2 a=1; b=1;
+ #2 $stop;
+end
+endmodule 
+ 
