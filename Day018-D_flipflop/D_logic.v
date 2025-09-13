9e9324fc150d3a188fa3145586287fed56704082
manish kushwaha <kushwahamanish13068@gmail.com>
2025-09-13 22:18:15 +0530

    Create D_logic.v

---
+++ Day018-D_flipflop/D_logic.v
@@ -0,0 +1,22 @@
+`timescale 1ns / 1ps
+
+module D_flipflop(D,Q,Qbar,clk,rst);
+ input D;
+ input clk,rst;
+ output reg Q;
+ output Qbar;
+ assign Qbar = ~Q;
+ 
+ always @(posedge clk or posedge rst)
+ begin 
+  if(rst) Q <= 0;
+  else 
+  begin 
+   if(D==0 | D==1)  Q <= D;
+   else  Q <= 1'bx;
+     
+  end 
+ end 
+ 
+
+endmodule
