feca4ba8d0483c6310994ef5f7ac7686189d2817
manish kushwaha <kushwahamanish13068@gmail.com>
2025-09-15 00:20:20 +0530

    Create testbench.v

---
+++ Day019-FSM(mealy_machine)/testbench.v
@@ -0,0 +1,28 @@
+module tb( );
+reg in,clk;
+wire out;
+detecter_1011 detect (in,out,clk);
+initial begin 
+ clk=0;
+ forever #5 clk=~clk;
+end
+initial begin
+ #12 in=1;
+ #10 in = 0; 
+ #10 in = 1; 
+ #10 in = 0; 
+ #10 in = 1; 
+ #10 in = 1; 
+ #10 in = 1;
+ #10 in = 1;
+ #10 in = 0;
+ #10 in = 1;
+ #10 in = 1;
+ #10 in = 0;
+ #10 in = 1;
+ #10 in = 1;
+ 
+ #50 $finish;
+  
+end 
+endmodule
